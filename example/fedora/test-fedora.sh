2235a0d706f19add59124dad5bd3852f87d08acf
131250106 <7438696+131250106@users.noreply.github.com>
2018-10-01 11:07:39 +0800

    Add files via upload

---
+++ example/fedora/test-fedora.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+Enfuzzer-host file -n fedora-27 -f boringssl.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f c-ares.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f in.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f guetzli.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f lcms.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f libarchive.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f libssh.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f libxml.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f openssl.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f pcre.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f proj4.tar.gz -d /
+
+Enfuzzer-host file -n fedora-27 -f re2.tar.gz -d /
+
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz boringssl.tar.gz" > 1.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz c-ares.tar.gz" > 2.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz guetzli.tar.gz" > 3.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz lcms.tar.gz" > 4.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz libarchive.tar.gz" > 5.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz libssh.tar.gz" > 6.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz libxml.tar.gz" > 7.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz openssl.tar.gz" > 8.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz pcre.tar.gz" > 9.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz proj4.tar.gz" > 10.log 2>&1
+
+Enfuzzer-host execute -n fedora-27 -i "/opt/enfuzzer/Enfuzzer build -gz re2.tar.gz" > 11.log 2>&1
+
